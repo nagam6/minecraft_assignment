04a91dbe6fc4f8a09b1655f3f816b771cd4a9c24
nagam6 <nagamdaban6@gmail.com>
2026-06-22 14:40:02 +0300

    Render world from matrix using DOM

--- js/script.js
+++ js/script.js
@@ -33,3 +33,36 @@ closeModalBtn.addEventListener("click", function () {
   playClickSound();
   howToPlayModal.classList.remove("active");
 });
+
+const worldMatrix = [
+  ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
+  ["sky","sky","sky","sky","leaf","leaf","sky","sky","sky","sky"],
+  ["sky","sky","sky","sky","wood","wood","sky","sky","sky","sky"],
+  ["sky","sky","sky","sky","wood","wood","sky","sky","sky","sky"],
+  ["grass","grass","grass","grass","grass","grass","grass","grass","grass","grass"],
+  ["dirt","dirt","dirt","stone","stone","dirt","dirt","dirt","stone","stone"],
+  ["dirt","dirt","stone","stone","stone","dirt","dirt","stone","stone","stone"]
+];
+
+function renderWorld() {
+  const world = document.querySelector("#world");
+
+  world.innerHTML = "";
+
+  worldMatrix.forEach((row, rowIndex) => {
+    row.forEach((tileType, colIndex) => {
+
+      const tile = document.createElement("div");
+
+      tile.classList.add("tile");
+      tile.classList.add(tileType);
+
+      tile.dataset.row = rowIndex;
+      tile.dataset.col = colIndex;
+
+      world.appendChild(tile);
+    });
+  });
+}
+
+renderWorld();
