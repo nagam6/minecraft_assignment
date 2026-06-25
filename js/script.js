6549aaf3ae47a6910f4016cc980cc9b1cbc5aec3
nagam6 <nagamdaban6@gmail.com>
2026-06-25 13:48:52 +0300

    Build game world

--- js/script.js
+++ js/script.js
@@ -35,13 +35,20 @@ closeModalBtn.addEventListener("click", function () {
 });
 
 const worldMatrix = [
-  ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-  ["sky","sky","sky","sky","leaf","leaf","sky","sky","sky","sky"],
-  ["sky","sky","sky","sky","wood","wood","sky","sky","sky","sky"],
-  ["sky","sky","sky","sky","wood","wood","sky","sky","sky","sky"],
-  ["grass","grass","grass","grass","grass","grass","grass","grass","grass","grass"],
-  ["dirt","dirt","dirt","stone","stone","dirt","dirt","dirt","stone","stone"],
-  ["dirt","dirt","stone","stone","stone","dirt","dirt","stone","stone","stone"]
+  ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","cloud","cloud","sky"],
+  ["sky","sky","sky","sky","cloud","cloud","sky","sky","sky","sky","sky","cloud","cloud","sky","sky","sky","sky","sky","cloud","cloud","cloud","cloud"],
+  ["sky","sky","sky","sky","sky","sky","cloud","cloud","sky","sky","cloud","cloud","cloud","cloud","sky","sky","sky","sky","sky","sky","sky","sky"],
+  ["cloud","cloud","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","leef","leef","leef","sky","sky","sky","sky"],
+  ["sky","sky","cloud","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","leef","leef","leef","leef","leef","sky","sky","sky"],
+  ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","wood","sky","sky","sky","sky","sky"],
+  ["sky","sky","sky","sky","sky","sky","sky","stone","sky","sky","sky","sky","sky","sky","sky","sky","wood","sky","sky","sky","sky","stone"],
+  ["sky","sky","sky","sky","sky","sky","stone","stone","stone","sky","sky","sky","sky","sky","sky","sky","wood","sky","sky","sky","stone","stone"],
+  ["grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass"],
+  ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt"],
+  ["green_diamond","green_diamond","stone","green_diamond","green_diamond","stone","green_diamond","green_diamond","stone","green_diamond","green_diamond","green_diamond","green_diamond","stone","green_diamond","green_diamond","green_diamond","green_diamond","green_diamond","stone","green_diamond","stone"], 
+  ["red_diamond","stone","red_diamond","red_diamond","stone","red_diamond","red_diamond","red_diamond","red_diamond","red_diamond","red_diamond","stone","red_diamond","red_diamond","red_diamond","red_diamond","red_diamond","stone","red_diamond","red_diamond","stone","red_diamond"],
+  ["diamond","diamond","diamond","stone","diamond","diamond","diamond","stone","diamond","diamond","diamond","diamond","diamond","stone","diamond","diamond","diamond","diamond","diamond","stone","diamond","stone"],
+  ["yellow_diamond","stone","yellow_diamond","yellow_diamond","stone","yellow_diamond","yellow_diamond","yellow_diamond","stone","yellow_diamond","yellow_diamond","yellow_diamond","stone","yellow_diamond","yellow_diamond","yellow_diamond","yellow_diamond","yellow_diamond","stone","yellow_diamond","yellow_diamond","stone"],
 ];
 
 function renderWorld() {
